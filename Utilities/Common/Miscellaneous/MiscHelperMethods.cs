742151b8b713f2c32efb8a96bcc890b97d858bd4
AlexanderMorou_cp <SND\AlexanderMorou_cp>
2011-11-21 00:21:19 +0000

    Took some time to condense the multi-keyed dictionary logic into the old code generation framework, seen from OILexer changeset 77291, to expedite making the proper setup.  Performance is equal to using multiple Dictionary<TKey1, Dictionary<TKey2, ...<TKeyN, TValue>... but the type looks much cleaner and is simpler to use (versus multiple TryGetValue calls and a final Add.)

--- Utilities/Common/Miscellaneous/MiscHelperMethods.cs
+++ Utilities/Common/Miscellaneous/MiscHelperMethods.cs
@@ -209,6 +209,148 @@ namespace AllenCopeland.Abstraction.Utilities.Miscellaneous
             return Tuple.Create(sw.Elapsed, result);
         }
 
+        public static IEnumerable<Tuple<int, int>> Iterate(int start1, int start2, int end1, int end2, int step1 = 1, int step2 = 1)
+        {
+            if (end1 > start1 && step1 > 0)
+                step1 = -step1;
+            if (end2 > start2 && step2 > 0)
+                step2 = -step2;
+            for (int i = start1; i <= end1; i += step1)
+                for (int j = start2; j < end2; j += step2)
+                    yield return new Tuple<int, int>(i, j);
+        }
+
+        public static IEnumerable<Tuple<int, int, int>> Iterate(int start1, int start2, int start3, int end1, int end2, int end3, int step1 = 1, int step2 = 1, int step3 = 1)
+        {
+            if (end1 > start1 && step1 > 0)
+                step1 = -step1;
+            if (end2 > start2 && step2 > 0)
+                step2 = -step2;
+            if (end3 > start3 && step3 > 0)
+                step3 = -step3;
+            for (int i = start1; i <= end1; i += step1)
+                for (int j = start2; j < end2; j += step2)
+                    for (int k = start3; k < end3; k++)
+                        yield return new Tuple<int, int, int>(i, j, k);
+        }
+
+
+        public static IEnumerable<Tuple<int, int, int, int>> Iterate(int start1, int start2, int start3, int start4, int end1, int end2, int end3, int end4, int step1 = 1, int step2 = 1, int step3 = 1, int step4 = 1)
+        {
+            if (end1 > start1 && step1 > 0)
+                step1 = -step1;
+            if (end2 > start2 && step2 > 0)
+                step2 = -step2;
+            if (end3 > start3 && step3 > 0)
+                step3 = -step3;
+            if (end4 > start4 && step4 > 0)
+                step4 = -step4;
+            for (int i = start1; i <= end1; i += step1)
+                for (int j = start2; j < end2; j += step2)
+                    for (int k = start3; k < end3; k++)
+                        for (int l = start4; l < end4; l++)
+                            yield return new Tuple<int, int, int, int>(i, j, k, l);
+        }
+
+        public static IEnumerable<Tuple<int, int, int, int, int>> Iterate(int start1, int start2, int start3, int start4, int start5, int end1, int end2, int end3, int end4, int end5, int step1 = 1, int step2 = 1, int step3 = 1, int step4 = 1, int step5 = 1)
+        {
+            if (end1 > start1 && step1 > 0)
+                step1 = -step1;
+            if (end2 > start2 && step2 > 0)
+                step2 = -step2;
+            if (end3 > start3 && step3 > 0)
+                step3 = -step3;
+            if (end4 > start4 && step4 > 0)
+                step4 = -step4;
+            if (end5 > start5 && step5 > 0)
+                step5 = -step5;
+            for (int i = start1; i <= end1; i += step1)
+                for (int j = start2; j < end2; j += step2)
+                    for (int k = start3; k < end3; k++)
+                        for (int l = start4; l < end4; l++)
+                            for (int m = start4; m < end4; m++)
+                                yield return new Tuple<int, int, int, int, int>(i, j, k, l, m);
+        }
+
+        public static IEnumerable<Tuple<int, int, int, int, int, int>> Iterate(int start1, int start2, int start3, int start4, int start5, int start6, int end1, int end2, int end3, int end4, int end5, int end6, int step1 = 1, int step2 = 1, int step3 = 1, int step4 = 1, int step5 = 1, int step6 = 1)
+        {
+            if (end1 > start1 && step1 > 0)
+                step1 = -step1;
+            if (end2 > start2 && step2 > 0)
+                step2 = -step2;
+            if (end3 > start3 && step3 > 0)
+                step3 = -step3;
+            if (end4 > start4 && step4 > 0)
+                step4 = -step4;
+            if (end5 > start5 && step5 > 0)
+                step5 = -step5;
+            if (end6 > start6 && step6 > 0)
+                step6 = -step6;
+            for (int i = start1; i <= end1; i += step1)
+                for (int j = start2; j < end2; j += step2)
+                    for (int k = start3; k < end3; k += step3)
+                        for (int l = start4; l < end4; l += step4)
+                            for (int m = start4; m < end4; m += step5)
+                                for (int n = start3; n < end3; n += step6)
+                                    yield return new Tuple<int, int, int, int, int, int>(i, j, k, l, m, n);
+        }
+
+
+        public static IEnumerable<Tuple<int, int, int, int, int, int, int>> Iterate(int start1, int start2, int start3, int start4, int start5, int start6, int start7, int end1, int end2, int end3, int end4, int end5, int end6, int end7, int step1 = 1, int step2 = 1, int step3 = 1, int step4 = 1, int step5 = 1, int step6 = 1, int step7 = 1)
+        {
+            if (end1 > start1 && step1 > 0)
+                step1 = -step1;
+            if (end2 > start2 && step2 > 0)
+                step2 = -step2;
+            if (end3 > start3 && step3 > 0)
+                step3 = -step3;
+            if (end4 > start4 && step4 > 0)
+                step4 = -step4;
+            if (end5 > start5 && step5 > 0)
+                step5 = -step5;
+            if (end6 > start6 && step6 > 0)
+                step6 = -step6;
+            if (end7 > start7 && step7 > 0)
+                step7 = -step7;
+            for (int i = start1; i <= end1; i += step1)
+                for (int j = start2; j < end2; j += step2)
+                    for (int k = start3; k < end3; k+=step3)
+                        for (int l = start4; l < end4; l+=step4)
+                            for (int m = start4; m < end4; m+=step5)
+                                for (int n = start3; n < end3; n+=step6)
+                                    for (int o = start4; o < end4; o+=step7)
+                                        yield return new Tuple<int, int, int, int, int, int, int>(i, j, k, l, m, n, o);
+        }
+
+        public static IEnumerable<Tuple<int, int, int, int, int, int, int, int>> Iterate(int start1, int start2, int start3, int start4, int start5, int start6, int start7, int start8, int end1, int end2, int end3, int end4, int end5, int end6, int end7, int end8, int step1 = 1, int step2 = 1, int step3 = 1, int step4 = 1, int step5 = 1, int step6 = 1, int step7 = 1, int step8 = 1)
+        {
+            if (end1 > start1 && step1 > 0)
+                step1 = -step1;
+            if (end2 > start2 && step2 > 0)
+                step2 = -step2;
+            if (end3 > start3 && step3 > 0)
+                step3 = -step3;
+            if (end4 > start4 && step4 > 0)
+                step4 = -step4;
+            if (end5 > start5 && step5 > 0)
+                step5 = -step5;
+            if (end6 > start6 && step6 > 0)
+                step6 = -step6;
+            if (end7 > start7 && step7 > 0)
+                step7 = -step7;
+            if (end8 > start8 && step8 > 0)
+                step8 = -step8;
+            for (int i = start1; i <= end1; i += step1)
+                for (int j = start2; j < end2; j += step2)
+                    for (int k = start3; k < end3; k += step3)
+                        for (int l = start4; l < end4; l += step4)
+                            for (int m = start4; m < end4; m += step5)
+                                for (int n = start3; n < end3; n += step6)
+                                    for (int o = start4; o < end4; o += step7)
+                                        for (int p = start4; p < end4; p += step8)
+                                            yield return new Tuple<int, int, int, int, int, int, int, int>(i, j, k, l, m, n, o, p);
+        }
+
         /// <summary>
         /// Times the <paramref name="func"/> with the <paramref name="arg"/>
         /// provided and and yields the <see cref="TimeSpan"/> and the
